5bb0ac301d4fbbf1c4f18d6984f08835c650d17c
Tanya <tanya.griz89@gmail.com>
2023-12-28 10:36:19 -0600

    start project

---
+++ dist/src_pages_AboutPage_AboutPage_tsx.f8726c2b1046f68d93d2.js
@@ -0,0 +1,26 @@
+"use strict";
+(self["webpackChunkfront-end-project"] = self["webpackChunkfront-end-project"] || []).push([["src_pages_AboutPage_AboutPage_tsx"],{
+
+/***/ "./src/pages/AboutPage/AboutPage.tsx":
+/*!*******************************************!*\
+  !*** ./src/pages/AboutPage/AboutPage.tsx ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   AboutPage: () => (/* binding */ AboutPage),
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
+
+var AboutPage = function () {
+    return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "About Page" }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LmY4NzI2YzJiMTA0NmY2OGQ5M2QyLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7OztBQUFnRDtBQUN6QztBQUNQLFlBQVksc0RBQUksVUFBVSx3QkFBd0I7QUFDbEQ7QUFDQSxpRUFBZSxTQUFTLEVBQUMiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9zcmMvcGFnZXMvQWJvdXRQYWdlL0Fib3V0UGFnZS50c3giXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsganN4IGFzIF9qc3ggfSBmcm9tIFwicmVhY3QvanN4LXJ1bnRpbWVcIjtcbmV4cG9ydCB2YXIgQWJvdXRQYWdlID0gZnVuY3Rpb24gKCkge1xuICAgIHJldHVybiAoX2pzeChcImRpdlwiLCB7IGNoaWxkcmVuOiBcIkFib3V0IFBhZ2VcIiB9KSk7XG59O1xuZXhwb3J0IGRlZmF1bHQgQWJvdXRQYWdlO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
